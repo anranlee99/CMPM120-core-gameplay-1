bb141a51de678ba387a966eec61af04388d36a8d
anranlee99 <anranlee99@gmail.com>
2023-05-28 16:14:28 -0700

    moves files to this repo

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  let basePath;
+  if (env["GITHUB_ACTIONS"] === "true") {
+    basePath = "/" + env["GITHUB_REPOSITORY"].split("/")[1];
+  } else {
+    basePath = "/";
+  }
+  return {
+    build: {
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        input: {
+          app: './core-gameplay-1.html',
+        },
+      },
+    },
+    server: {
+      open: '/core-gameplay-1.html',
+    },
+    base: basePath
+  }
+})
